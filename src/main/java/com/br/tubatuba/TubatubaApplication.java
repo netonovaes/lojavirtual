064c4e66d0a19222479f3fddf11fa16061dd63ca
netonovaes <netonovaes@gmail.com>
2020-07-06 10:56:18 -0300

    Versao de criacao do projeto Loja Virtual

---
+++ src/main/java/com/br/tubatuba/TubatubaApplication.java
@@ -0,0 +1,12 @@
+package com.br.tubatuba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubatubaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubatubaApplication.class, args);
+	}
+} 
