d0c386b40f1dd2a026e40efcc4c9e03f89838fb5
netonovaes <netonovaes@gmail.com>
2020-07-06 14:46:08 -0300

    Implementando muitos para muitos entre categoria e produto

--- src/main/java/com/br/lojavirtual/LojaVirtualApplication.java
+++ src/main/java/com/br/lojavirtual/LojaVirtualApplication.java
@@ -1,21 +1,24 @@
 package com.br.lojavirtual;
 
 import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.br.lojavirtual.domain.Categoria;
+import com.br.lojavirtual.domain.Produto;
 import com.br.lojavirtual.repositories.CategoriaRepository;
+import com.br.lojavirtual.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class LojaVirtualApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualApplication.class, args);
 	}
@@ -26,8 +29,22 @@ public class LojaVirtualApplication implements CommandLineRunner{
 		  Categoria cat1 = new Categoria(null,"Informatica");
 		  Categoria cat2 = new Categoria(null,"Escritorio");
 		  
-		  categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		 
+		  Produto p1 = new Produto(null,"Computador", 2000.00);
+		  Produto p2 = new Produto(null,"Impressora", 800.00);
+		  Produto p3 = new Produto(null,"Mouse", 80.00);
+		 
+		  
+		  //Implementacao do diagrama de instancia com relacionamento muitos para muitos entre categoria e produto
+		  cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		  cat2.getProdutos().addAll(Arrays.asList(p2));
+		  
+		  p1.getCategorias().addAll(Arrays.asList(cat1));
+		  p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		  p3.getCategorias().addAll(Arrays.asList(cat1));
+	
+		  categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		  produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 		
 } 
--- src/main/java/com/br/lojavirtual/domain/Produto.java
+++ src/main/java/com/br/lojavirtual/domain/Produto.java
@@ -3,57 +3,39 @@ package com.br.lojavirtual.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-@Entity
-public class Categoria  implements Serializable{
-	private static final long serialVersionUID = 1L;
 
+@Entity
+public class Produto  implements Serializable{
+	private static final long serialVersionUID = 1L;
+   
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
 	private String nome;
+	private Double  preco;
+	//Relacionamento muitos para muitos de categoria e produto
+    @ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",joinColumns = @JoinColumn(name = "produto_id"),inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	//Relacionamento muitos para muitos de categoria e produto(apenas apontando para o mapeio ManyToMany em Produto)
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
-	
-	
-	
-	public Categoria() {
+	public Produto() {
 		
 	}
-	public Categoria(Integer id, String nome) {
-		
+	public Produto(Integer id, String nome, Double preco) {
+		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
-	
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-    
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
-	}
-	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -61,6 +43,7 @@ public class Categoria  implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -69,7 +52,7 @@ public class Categoria  implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -78,4 +61,23 @@ public class Categoria  implements Serializable{
 		return true;
 	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+
+	
+	
 }
