c654498127bb9170b4d087f3c1613f897a0a57ed
netonovaes <netonovaes@gmail.com>
2020-07-08 14:28:10 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/br/lojavirtual/resources/CategoriaResource.java
+++ src/main/java/com/br/lojavirtual/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.br.lojavirtual.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.br.lojavirtual.domain.Categoria;
+import com.br.lojavirtual.dto.CategoriaDTO;
 import com.br.lojavirtual.services.CategoriaService;
 
 @RestController
@@ -45,10 +48,20 @@ public class CategoriaResource {
 		return  ResponseEntity.noContent().build();
 		
 	}
+	//Atualizando categoria com DELETE
 	@RequestMapping(value ="/{id}",method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return  ResponseEntity.noContent().build();	
 	}
+	//Buscando uma lista de categorias 
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());//converte uma lista para outra lista
+		return ResponseEntity.ok().body(listDto);
+	}	
+	
+	
 }
 
--- src/main/java/com/br/lojavirtual/services/CategoriaService.java
+++ src/main/java/com/br/lojavirtual/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.br.lojavirtual.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,8 +45,10 @@ public class CategoriaService {
 	    throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");	 
 	 
 	 }
+    }
+	public List<Categoria> findAll(){
+		return repo.findAll();
 		
-		
-	 }
+	}
 	
 }
