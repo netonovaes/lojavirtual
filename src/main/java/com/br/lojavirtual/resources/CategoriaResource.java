5ed8fd632354ff0461eaaab86eb12100608742d9
netonovaes <netonovaes@gmail.com>
2020-07-08 11:44:01 -0300

    Atualizando categoria com PUT

--- src/main/java/com/br/lojavirtual/resources/CategoriaResource.java
+++ src/main/java/com/br/lojavirtual/resources/CategoriaResource.java
@@ -23,11 +23,12 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping(value ="/{id}",method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}	
 	
+	//Inserindo categoria com POST
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
 		obj = service.insert(obj);
@@ -35,4 +36,15 @@ public class CategoriaResource {
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+
+	//Atualizando categoria com PUT (primeiro recebe o objeto para depois atualizar
+	@RequestMapping(value ="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @	PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return  ResponseEntity.noContent().build();
+		
+	}
+	
 }
+
--- src/main/java/com/br/lojavirtual/services/CategoriaService.java
+++ src/main/java/com/br/lojavirtual/services/CategoriaService.java
@@ -3,7 +3,6 @@ package com.br.lojavirtual.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.br.lojavirtual.domain.Categoria;
@@ -21,11 +20,15 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id); 
 		return obj.orElseThrow(() -> new ObjectNotFoundException(    "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
 		}
-	
+	//Metodo de inserir
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+	// Metodo updade
+	public Categoria update(Categoria obj) {
+		find(obj.getId());// verifica se o objeto existe e lança uma exceção  personalizada do metodo find
+		return repo.save(obj);
+	}
 	
 }
